d47d8cd8582edce5a6c216e7efff2f0bfce65e5f
bala-amypo <bala@amypo.in>
2025-12-21 10:02:04 +0000

    User commit

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -1,26 +1,16 @@
 package com.example.demo.model;
 
-import jakarta.persistence.*;
-import lombok.Data;
-
-import java.time.LocalDateTime;
-
-@Data
-@Entity
-@Table(name = "products")
 public class Product {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
     private String productName;
 
-    private String sku;
+    // getter
+    public String getProductName() {
+        return productName;
+    }
 
-    private String description;
-
-    private Double price;
-
-    private LocalDateTime createdAt;
+    // setter
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
 }
