7b2130a9846ed2129aa0a672695a9f5857fd2706
bala-amypo <bala@amypo.in>
2025-12-21 10:05:04 +0000

    User commit

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -1,16 +1,47 @@
 package com.example.demo.model;
 
+import java.time.LocalDateTime;
+
 public class Product {
 
+    private Long id;
     private String productName;
+    private String sku;
+    private LocalDateTime createdAt;
 
-    // getter
+    // getter & setter for id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // getter & setter for productName
     public String getProductName() {
         return productName;
     }
 
-    // setter
     public void setProductName(String productName) {
         this.productName = productName;
+    }
+
+    // getter & setter for sku
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    // getter & setter for createdAt
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
     }
 }
