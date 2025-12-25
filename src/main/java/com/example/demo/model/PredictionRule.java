7fe06fc680e0279eb2309cb2df898e1af6860b30
bala-amypo <bala@amypo.in>
2025-12-25 15:35:02 +0000

    User commit

--- src/main/java/com/example/demo/model/PredictionRule.java
+++ src/main/java/com/example/demo/model/PredictionRule.java
@@ -1,8 +1,61 @@
 package com.example.demo.model;
 
-public class PredictionRule {
-    private String rule;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-    public String getRule() { return rule; }
-    public void setRule(String rule) { this.rule = rule; }
+@Entity
+@Table(name = "prediction_rules")
+public class PredictionRule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productCategory;
+    private Integer minQuantity;
+    private Integer reorderQuantity;
+
+    public PredictionRule() {
+    }
+
+    public PredictionRule(String productCategory, Integer minQuantity, Integer reorderQuantity) {
+        this.productCategory = productCategory;
+        this.minQuantity = minQuantity;
+        this.reorderQuantity = reorderQuantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(String productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public Integer getMinQuantity() {
+        return minQuantity;
+    }
+
+    public void setMinQuantity(Integer minQuantity) {
+        this.minQuantity = minQuantity;
+    }
+
+    public Integer getReorderQuantity() {
+        return reorderQuantity;
+    }
+
+    public void setReorderQuantity(Integer reorderQuantity) {
+        this.reorderQuantity = reorderQuantity;
+    }
 }
