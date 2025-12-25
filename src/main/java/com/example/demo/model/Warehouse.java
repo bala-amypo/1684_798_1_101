e5eaa1bfa5894007774cc03fbb9edbaef7bfba51
bala-amypo <bala@amypo.in>
2025-12-25 15:44:02 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Warehouse.java
@@ -0,0 +1,52 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "warehouses")
+public class Warehouse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String location;
+
+    public Warehouse() {
+    }
+
+    public Warehouse(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
