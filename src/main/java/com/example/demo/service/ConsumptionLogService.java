e471879a7f90e0786158c65d6e63b8e7a65d0a4c
bala-amypo <bala@amypo.in>
2025-12-25 09:00:13 +0000

    User commit

--- src/main/java/com/example/demo/service/ConsumptionLogService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.ConsumptionLog;
-
-public interface ConsumptionLogService {
-    ConsumptionLog logConsumption(ConsumptionLog consumptionLog);
-}
--- src/main/java/com/example/demo/service/PredictionService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-public interface PredictionService {
-    int predictStockOutDays(Long stockRecordId);
-    double calculateAverageDailyConsumption(Long stockRecordId, int days);
-    boolean isStockCritical(Long stockRecordId);
-    int calculateReorderQuantity(Long stockRecordId);
-}
