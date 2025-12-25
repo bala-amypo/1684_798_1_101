66cc172834f5bd460f4cd4ec932f191acf3f1a42
bala-amypo <bala@amypo.in>
2025-12-25 07:15:12 +0000

    User commit

--- src/main/java/com/example/demo/model/Warehouse.java
+++ src/main/java/com/example/demo/model/Warehouse.java
@@ -1,24 +1,13 @@
-package com.example.demo.model;
-
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "warehouses")
 public class Warehouse {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     @Column(nullable = false)
     private String name;
-    
-    private String location;
 }
